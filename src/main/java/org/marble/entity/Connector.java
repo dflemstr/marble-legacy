b022e42f8fe5da1f4bbcd69e44dfd25298816c34
David Flemström <david.flemstrom@gmail.com>
2012-03-20 14:21:51 +0100

    Remove a superfluous mirror matrix when joining connectors

--- src/main/java/org/marble/entity/Connector.java
+++ src/main/java/org/marble/entity/Connector.java
@@ -2,22 +2,22 @@ package org.marble.entity;
 
 import javax.vecmath.Matrix4f;
 
+import com.google.common.base.Objects;
+
 public class Connector {
     private final Matrix4f offset;
-    private static final Matrix4f mirrorX = new Matrix4f();
-
-    static {
-        mirrorX.setIdentity();
-        mirrorX.setElement(0, 0, -1);
-    }
 
     public Connector(final Matrix4f offset) {
         this.offset = offset;
     }
 
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).add("offset", offset).toString();
+    }
+
     public void transformInto(final Connector that, final Matrix4f out) {
         out.invert(that.offset);
-        out.mul(mirrorX);
         out.mul(offset, out);
     }
 }
