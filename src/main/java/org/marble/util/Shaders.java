600b55819c3cc83c2df3dd44edf8fb3b1dffff91
David Flemström <david.flemstrom@gmail.com>
2012-03-26 02:05:22 +0200

    Keep track of how often each shader is loaded

--- src/main/java/org/marble/util/Shaders.java
+++ src/main/java/org/marble/util/Shaders.java
@@ -1,9 +1,12 @@
 package org.marble.util;
 
 import java.io.IOException;
+import java.util.Map;
 
 import com.ardor3d.renderer.state.GLSLShaderObjectsState;
 import com.ardor3d.util.resource.ResourceLocatorTool;
+
+import com.google.common.collect.Maps;
 
 import org.marble.Game;
 
@@ -11,6 +14,8 @@ public final class Shaders {
     private static final String SHADER_DIR = Game.class.getPackage().getName()
             .replace('.', '/')
             + "/shader/";
+    private static final Map<String, Integer> shaderLoadCount = Maps
+            .newHashMap();
 
     public static GLSLShaderObjectsState loadShader(final String name) {
         final GLSLShaderObjectsState shader = new GLSLShaderObjectsState();
@@ -25,8 +30,16 @@ public final class Shaders {
         } catch (final IOException e) {
             throw new RuntimeException(e);
         }
-        shader._fragmentShaderName = name;
-        shader._vertexShaderName = name;
+
+        int loadCount =
+                shaderLoadCount.containsKey(name) ? shaderLoadCount.get(name)
+                        : 0;
+        loadCount++;
+
+        shader._fragmentShaderName = name + loadCount;
+        shader._vertexShaderName = name + loadCount;
+
+        shaderLoadCount.put(name, loadCount);
 
         return shader;
     }
