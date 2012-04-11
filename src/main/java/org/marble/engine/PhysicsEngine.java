916d23c42fbf43ade000ea10479c5ec1d61685d0
David Flemström <david.flemstrom@gmail.com>
2012-04-11 23:40:56 +0200

    Fix physics engine stepping

--- src/main/java/org/marble/engine/PhysicsEngine.java
+++ src/main/java/org/marble/engine/PhysicsEngine.java
@@ -109,7 +109,7 @@ public class PhysicsEngine extends Engine<Physical> {
             body.setWorldTransform(worldTransform);
         }
 
-        world.stepSimulation((float) timer.getTimePerFrame(), 4, 1f / (30 * 4));
+        world.stepSimulation((float) timer.getTimePerFrame(), 8);
         return true;
     }
 }
