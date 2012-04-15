d17b04a6f1072f63f586051a09bee7e063ed748c
David Flemström <david.flemstrom@gmail.com>
2012-04-15 14:15:39 +0200

    Optimize scene traversal

--- src/main/java/org/marble/graphics/scene/PostIlluminationVisitor.java
+++ src/main/java/org/marble/graphics/scene/PostIlluminationVisitor.java
@@ -1,14 +1,18 @@
 package org.marble.graphics.scene;
 
+import com.ardor3d.renderer.Renderer;
 import com.ardor3d.scenegraph.Spatial;
 
 public class PostIlluminationVisitor extends AbstractRenderVisitor {
 
     @Override
-    public void visit(final Spatial spatial) {
-        if (spatial instanceof PostIllumination) {
-            ((PostIllumination) spatial).postIllumination(getRenderer());
-        }
+    boolean shouldRender(final Spatial spatial) {
+        return spatial instanceof PostIllumination;
+    }
+
+    @Override
+    void render(final Spatial spatial, final Renderer renderer) {
+        ((PostIllumination) spatial).postIllumination(renderer);
     }
 
 }
--- src/main/java/org/marble/graphics/scene/PreDrawingVisitor.java
+++ src/main/java/org/marble/graphics/scene/PreDrawingVisitor.java
@@ -1,14 +1,18 @@
 package org.marble.graphics.scene;
 
+import com.ardor3d.renderer.Renderer;
 import com.ardor3d.scenegraph.Spatial;
 
 public class PreDrawingVisitor extends AbstractRenderVisitor {
 
     @Override
-    public void visit(final Spatial spatial) {
-        if (spatial instanceof PreDrawing) {
-            ((PreDrawing) spatial).preDraw(getRenderer());
-        }
+    boolean shouldRender(final Spatial spatial) {
+        return spatial instanceof PreDrawing;
+    }
+
+    @Override
+    void render(final Spatial spatial, final Renderer renderer) {
+        ((PreDrawing) spatial).preDraw(renderer);
     }
 
 }
