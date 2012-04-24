cb20915ca9a898d3d45c821beb9c5ab66b304cbf
David Flemström <david.flemstrom@gmail.com>
2012-04-24 17:59:20 +0200

    Add support for ghost sensors

--- src/main/java/org/marble/engine/PhysicsEngine.java
+++ src/main/java/org/marble/engine/PhysicsEngine.java
@@ -9,6 +9,7 @@ import com.jme3.bullet.PhysicsSpace.BroadphaseType;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.control.GhostControl;
 import com.jme3.math.Vector3f;
 import com.jme3.system.JmeContext;
 
@@ -18,15 +19,19 @@ import com.google.common.collect.Sets;
 import org.marble.entity.physical.Actor;
 import org.marble.entity.physical.Collidable;
 import org.marble.entity.physical.Physical;
+import org.marble.entity.physical.Sensor;
+import org.marble.session.GameSession;
 
 /**
  * The JBullet-based physics engine.
  */
 public class PhysicsEngine extends Engine<Physical> {
     private PhysicsSpace physicsSpace;
+    private final Set<Physical> physicals = Sets.newIdentityHashSet();
     private final Map<PhysicsCollisionObject, Physical> associations = Maps
             .newIdentityHashMap();
     private final Set<Actor> actors = Sets.newIdentityHashSet();
+    private GameSession.PauseState pauseState = GameSession.PauseState.Running;
 
     public PhysicsEngine(final JmeContext context) {
         super(Physical.class);
@@ -72,28 +77,53 @@ public class PhysicsEngine extends Engine<Physical> {
 
     @Override
     public void update(final float timePerFrame) {
-        for (final Actor actor : actors) {
-            actor.performActions(timePerFrame);
+        if (pauseState == GameSession.PauseState.Running) {
+            for (final Physical entity : physicals) {
+                entity.getBody().setPhysicsLocation(
+                        entity.getTransform().getTranslation());
+                entity.getBody().setPhysicsRotation(
+                        entity.getTransform().getRotation());
+            }
+            for (final Actor actor : actors) {
+                actor.performActions(timePerFrame);
+            }
+            physicsSpace.update(timePerFrame);
+            physicsSpace.distributeEvents();
         }
-        physicsSpace.update(timePerFrame);
-        physicsSpace.distributeEvents();
     }
 
     @Override
     protected void entityAdded(final Physical entity) {
         physicsSpace.add(entity.getBody());
         associations.put(entity.getBody(), entity);
+        physicals.add(entity);
         if (entity instanceof Actor) {
             actors.add((Actor) entity);
+            if (entity instanceof Sensor) {
+                for (final GhostControl sensor : ((Sensor) entity).getSensors()) {
+                    physicsSpace.add(sensor);
+                }
+            }
         }
+    }
+
+    @Override
+    public void setPause(final GameSession.PauseState state) {
+        pauseState = state;
     }
 
     @Override
     protected void entityRemoved(final Physical entity) {
         physicsSpace.remove(entity.getBody());
         associations.remove(entity.getBody());
+        physicals.remove(entity);
         if (entity instanceof Actor) {
             actors.remove(entity);
+            if (entity instanceof Sensor) {
+                for (final GhostControl sensor : ((Sensor) entity).getSensors()) {
+                    physicsSpace.remove(sensor);
+                }
+            }
         }
     }
 }
