7b83f38820440cb0c54db03acdb48357137f4920
David Flemström <david.flemstrom@gmail.com>
2012-05-10 00:22:16 +0200

    Image fixes

--- src/main/java/org/marble/special/Image.java
+++ src/main/java/org/marble/special/Image.java
@@ -3,8 +3,10 @@ package org.marble.special;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
+import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Quad;
 import com.jme3.texture.Texture;
 
@@ -32,15 +34,16 @@ public class Image extends AbstractEntity implements Graphical {
         this.imageFile = imageFile;
         this.width = width;
         this.height = height;
-        this.angleX = angleX;
-        this.angleY = angleY;
-        this.angleZ = angleZ;
+        this.angleX = angleX * FastMath.DEG_TO_RAD;
+        this.angleY = angleY * FastMath.DEG_TO_RAD;
+        this.angleZ = angleZ * FastMath.DEG_TO_RAD;
     }
 
     @Override
     public void initialize(final Game game) {
         final AssetManager assetManager = game.getAssetManager();
         graphicalQuad = new Geometry("image", new Quad(width, height, true));
+        graphicalQuad.setLocalTranslation(-width / 2, -height / 2, 0);
         final Material mat =
                 new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
         final Texture tex = assetManager.loadTexture(imageFile);
@@ -49,9 +52,11 @@ public class Image extends AbstractEntity implements Graphical {
         mat.setFloat("AlphaDiscardThreshold", 0.05f);
         mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
         graphicalQuad.setMaterial(mat);
-        graphicalQuad.setLocalRotation(new Quaternion().fromAngles(angleX,
-                angleY, angleZ));
-        graphicalQuad.setLocalTranslation(-width / 2, -height / 2, 0.05f);
-        getSpatial().attachChild(graphicalQuad);
+
+        final Node helper = new Node("helper");
+        helper.attachChild(graphicalQuad);
+        helper.setLocalRotation(new Quaternion().fromAngles(angleX, angleY,
+                angleZ));
+        getSpatial().attachChild(helper);
     }
 }
